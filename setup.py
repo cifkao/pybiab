993520431ee999002f27ea5e62097b8985fb575a
Ondrej Cifka <cifkao@users.noreply.github.com>
2021-04-26 17:35:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+import sys
+
+setuptools.setup(
+    name='pybiab',
+    author='Ondřej Cífka',
+    author_email='cifkao@gmail.com',
+    description='Band-in-a-Box/RealBand automation',
+    url='https://github.com/cifkao/pybiab',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    python_requires='>=3.5',
+    install_requires=[
+        'pywinauto',
+        'pywin32',
+        'psutil',
+    ],
+)
